d897a8ae8b2d070ade51d8337e98bd1d8a867722
marcogonzalez99 <Mgonz781@mtroyal.ca>
2022-11-04 14:00:34 -0600

    format changes

--- Space_Invaders/space_invaders.py
+++ Space_Invaders/space_invaders.py
@@ -22,12 +22,14 @@ class Game:
         cols = level["cols"]
 
         # Player Setup
-        self.player_sprite = Player((screen_width/2, screen_height), screen_width, 5, self.level_num)
+        self.player_sprite = Player(
+            (screen_width/2, screen_height), screen_width, 5, self.level_num)
         self.player = pygame.sprite.GroupSingle(self.player_sprite)
 
         # Health and Score setup
         self.lives = 4
-        self.life_surface = pygame.image.load("Images/misc/player.png").convert_alpha()
+        self.life_surface = pygame.image.load(
+            "Images/misc/player.png").convert_alpha()
         self.score = 0
         self.font = pygame.font.Font('Pixeltype.ttf', 40)
 
@@ -43,7 +45,7 @@ class Game:
 
         # Alien Setup
         self.aliens = pygame.sprite.Group()
-        
+
         # Create the level 1 layout for the game
         self.alien_setup(rows, cols)
         self.alien_direction = self.alien_speed
@@ -55,7 +57,7 @@ class Game:
         # Extra - Bonus Alien
         self.extra = pygame.sprite.GroupSingle()
         self.extra_spawn_time = randint(400, 800)
-        
+
         # Win Condition
         self.win_timer = 0
 
@@ -83,11 +85,11 @@ class Game:
         for row_index, row in enumerate(range(rows)):
             # get alien colour for row
             alien_colour = aliens[row % len(aliens)]
-            
+
             for col_index, col in enumerate(range(cols)):
                 x = col_index * x_distance + x_offset
                 y = row_index * y_distance + y_offset
-                        
+
                 alien_sprite = Alien(alien_colour, x, y, self.level_num)
                 self.aliens.add(alien_sprite)
 
@@ -163,7 +165,8 @@ class Game:
 
     def display_lives(self):
         for index in range(self.lives - 1):
-            x = screen_width - (self.life_surface.get_width() * (index + 1)) - 10
+            x = screen_width - \
+                (self.life_surface.get_width() * (index + 1)) - 10
             screen.blit(self.life_surface, (x, 10))
 
     def display_score(self):
@@ -176,7 +179,7 @@ class Game:
         # stop music and play victory sound
         self.music.stop()
         self.win_fx.play()
-    
+
     def next_round(self):
         # This function manages the state of the round, allows us to access state and alter it easily
         if self.level_num == 5:
@@ -194,7 +197,8 @@ class Game:
 
         # Update player sprite
         self.player.remove(self.player_sprite)
-        player_sprite = Player((screen_width/2, screen_height), screen_width, 5, self.level_num)
+        player_sprite = Player(
+            (screen_width/2, screen_height), screen_width, 5, self.level_num)
         self.player = pygame.sprite.GroupSingle(player_sprite)
 
         # Update alien setup
@@ -213,27 +217,30 @@ class Game:
 
             elif self.win_timer < 100:
                 victory_surface = self.font.render('You Won', False, 'white')
-                
+
             elif 100 < self.win_timer < 425 and self.level_num == 5:
-                victory_surface = self.font.render('Congratulations', False, 'white')
-            
+                victory_surface = self.font.render(
+                    'Congratulations', False, 'white')
+
             elif 100 < self.win_timer < 200:
-                victory_surface = self.font.render('Next Level Starting', False, 'white')
-            
+                victory_surface = self.font.render(
+                    'Next Level Starting', False, 'white')
+
             elif 200 < self.win_timer < 275:
                 victory_surface = self.font.render('3', False, 'white')
-            
+
             elif 275 < self.win_timer < 350:
                 victory_surface = self.font.render('2', False, 'white')
-            
+
             elif 350 < self.win_timer < 425:
                 victory_surface = self.font.render('1', False, 'white')
-            
+
             elif self.win_timer > 425:
                 self.win_timer = 0
                 self.next_round()
 
-            victory_rect = victory_surface.get_rect(center=(screen_width/2, screen_height/2))
+            victory_rect = victory_surface.get_rect(
+                center=(screen_width/2, screen_height/2))
             screen.blit(victory_surface, victory_rect)
 
     def set_sounds(self):
@@ -253,14 +260,14 @@ class Game:
         self.explosion_sound.set_volume(0.1)
         self.win_fx = pygame.mixer.Sound(win_fx_path)
         self.win_fx.set_volume(0.15)
-                
+
     def run(self):
         # Updates
         self.player.update()
         self.extra.update()
         self.alien_lasers.update()
         self.aliens.update(self.alien_direction)
-        self.alien_position_checker() #Work on this to change the game speed
+        self.alien_position_checker()  # Work on this to change the game speed
         self.extra_alien_timer()
         self.collision_checks()
 
@@ -276,7 +283,7 @@ class Game:
 
         # Victory
         self.victory_timer()
-            
+
 
 class CRT:
     def __init__(self):
@@ -298,6 +305,7 @@ class CRT:
         self.create_crt_lines()
         screen.blit(self.tv, (0, 0))
 
+
 class GameState():
     def __init__(self):
         self.set_state("intro")
@@ -306,7 +314,8 @@ class GameState():
         self.state = state
         if self.state == "intro":
             # Intro text
-            self.intro_message = game_font.render("Click to Start", False, 'white')
+            self.intro_message = game_font.render(
+                "Click to Start", False, 'white')
             self.intro_rect = self.intro_message.get_rect(
                 center=(screen_width/2, screen_height/2 + 150))
 
@@ -314,20 +323,24 @@ class GameState():
             # Get specific level data
             bg_path = game.level_settings.get_value(game.level_num, "bg_path")
             self.bg = pygame.image.load(bg_path)
-            self.bg = pygame.transform.scale(self.bg, (screen_width, screen_height))
-        
+            self.bg = pygame.transform.scale(
+                self.bg, (screen_width, screen_height))
+
             # Level text
-            self.level_message = game_font.render(f"Level {game.level_num}", False, 'white')
+            self.level_message = game_font.render(
+                f"Level {game.level_num}", False, 'white')
             self.level_rect = self.level_message.get_rect(
                 center=(screen_width/2, 25))
 
         if self.state == "game_over":
             # Outro text
-            self.game_over_message = game_font.render("Game Over", False, 'white')
+            self.game_over_message = game_font.render(
+                "Game Over", False, 'white')
             self.game_over_rect = self.game_over_message.get_rect(
                 center=(screen_width/2, screen_height/2 + 100))
 
-            self.game_over_message_1 = game_font.render(f"Final Score: {game.score}", False, 'white')
+            self.game_over_message_1 = game_font.render(
+                f"Final Score: {game.score}", False, 'white')
             self.game_over_rect_1 = self.game_over_message_1.get_rect(
                 center=(screen_width/2, screen_height/2 + 125))
 
@@ -338,14 +351,14 @@ class GameState():
             self.main()
         if self.state == "game_over":
             self.game_over()
-    
+
     def intro(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                self.set_state("main") # start game
+                self.set_state("main")  # start game
 
         screen.fill((30, 30, 30))
         screen.blit(self.intro_message, self.intro_rect)
@@ -365,7 +378,7 @@ class GameState():
                 if event.key == pygame.K_UP:
                     game.aliens.sprites().clear()
                     game.aliens.empty()
-                
+
         screen.fill((30, 30, 30))
         screen.blit(self.bg, (0, 0))
         screen.blit(self.level_message, self.level_rect)
@@ -380,11 +393,12 @@ class GameState():
                 sys.exit()
 
         screen.fill((30, 30, 30))
-        screen.blit(game_logo,game_logo_rect)
+        screen.blit(game_logo, game_logo_rect)
         screen.blit(self.game_over_message, self.game_over_rect)
         screen.blit(self.game_over_message_1, self.game_over_rect_1)
         crt.draw()
         pygame.display.flip()
+
 
 if __name__ == '__main__':
     # General Setup
